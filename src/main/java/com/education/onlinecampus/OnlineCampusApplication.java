e49bef3f6fec94de8d3cdf20047c0c6e79cdd8f1
NNECT <wingggkr@gmail.com>
2023-08-05 14:48:15 +0900

    Initial commit

---
+++ src/main/java/com/education/onlinecampus/OnlineCampusApplication.java
@@ -0,0 +1,13 @@
+package com.education.onlinecampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/education/onlinecampus/OnlineCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.onlinecampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
