c7cbbaa8561dadd00b91f0d2ec5bc60a38de00ce
Kim-Byungju <xbj3812@gmail.com>
2023-08-07 17:51:28 +0900

    시큐리티 기본 로그인페이지없앰

--- src/main/java/com/education/onlinecampus/config/SecurityConfig.java
+++ src/main/java/com/education/onlinecampus/config/SecurityConfig.java
@@ -1,4 +1,3 @@
-/*
 package com.education.onlinecampus.config;
 
 import org.springframework.context.annotation.Bean;
@@ -14,13 +13,13 @@ public class SecurityConfig {
         // /login, /signup 페이지는 모두 허용, 다른 페이지는 인증된 사용자만 허용
         http
                 .authorizeRequests()
-                .antMatchers("/login", "/signup").permitAll()
+                .antMatchers("/Member_login", "/signup").permitAll()
                 .anyRequest().authenticated();
 
         // login 설정
         http
                 .formLogin()
-                .loginPage("/login")    // GET 요청 (login form을 보여줌)
+                .loginPage("/Member_login")    // GET 요청 (login form을 보여줌)
                 .loginProcessingUrl("/auth")    // POST 요청 (login 창에 입력한 데이터를 처리)
                 .usernameParameter("email")	// login에 필요한 id 값을 email로 설정 (default는 username)
                 .passwordParameter("password")	// login에 필요한 password 값을 password(default)로 설정
@@ -33,4 +32,4 @@ public class SecurityConfig {
                 .logoutSuccessUrl("/");	// logout에 성공하면 /로 redirect
         return http.build();
     }
-}*/
+}
