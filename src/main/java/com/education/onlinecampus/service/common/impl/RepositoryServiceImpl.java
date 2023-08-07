52940204b8bace291b022fe8d79dfcdd50e3e060
NNECT <wingggkr@gmail.com>
2023-08-07 17:19:24 +0900

    RepositoryServiceImpl 내 Getter 어노테이션 삭제

--- src/main/java/com/education/onlinecampus/service/common/impl/RepositoryServiceImpl.java
+++ src/main/java/com/education/onlinecampus/service/common/impl/RepositoryServiceImpl.java
@@ -3,17 +3,13 @@ package com.education.onlinecampus.service.common.impl;
 import com.education.onlinecampus.data.dto.*;
 import com.education.onlinecampus.data.entity.*;
 import com.education.onlinecampus.data.mapper.*;
-import com.education.onlinecampus.data.marker.DTOMarker;
-import com.education.onlinecampus.data.marker.EntityMarker;
-import com.education.onlinecampus.repository.FileRepository;
-import com.education.onlinecampus.repository.MemberRepository;
+import com.education.onlinecampus.data.marker.*;
+import com.education.onlinecampus.repository.*;
 import com.education.onlinecampus.service.common.RepositoryService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@Getter
 @RequiredArgsConstructor
 public class RepositoryServiceImpl implements RepositoryService {
     private final FileRepository fileRepository;
