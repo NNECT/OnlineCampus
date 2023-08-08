b2e2a2dc8a4e8a7428e3cbd314fc180966ca1dd2
Kim-Byungju <xbj3812@gmail.com>
2023-08-08 17:41:39 +0900

    rebase

--- src/main/java/com/education/onlinecampus/config/SecurityConfig.java
+++ src/main/java/com/education/onlinecampus/config/SecurityConfig.java
@@ -27,8 +27,8 @@ public class SecurityConfig{
                 .formLogin()
                 .loginPage("/Member_login")    // GET 요청 (login form을 보여줌)
                 .loginProcessingUrl("/Member_login") // POST 요청 (login 창에 입력한 데이터를 처리)
-                //.usernameParameter("username")	// login에 필요한 id 값을 email로 설정 (default는 username)
-                //.passwordParameter("password")	// login에 필요한 password 값을 password(default)로 설정
+                .usernameParameter("username")	// login에 필요한 id 값을 email로 설정 (default는 username)
+                .passwordParameter("password")	// login에 필요한 password 값을 password(default)로 설정
                 .defaultSuccessUrl("/Member_login") // login에 성공하면 /로 redirect*/
                 .permitAll();
 
