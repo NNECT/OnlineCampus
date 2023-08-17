7651305f7277e467705108ce50eee82e7085ec82
NNECT <wingggkr@gmail.com>
2023-08-17 17:24:40 +0900

    상수 세팅

---
+++ src/main/java/com/education/onlinecampus/config/Constant.java
@@ -0,0 +1,6 @@
+package com.education.onlinecampus.config;
+
+public class Constant {
+    public static final String APPLICATION_NAME = "OnlineCampus";
+    public static final String OAUTH_REDIRECT_URI = "http://localhost:8080/oauth2callback";
+}
