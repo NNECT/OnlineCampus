df9bb24822d043a5fed4a310691f504f8e757205
Kim-Byungju <xbj3812@gmail.com>
2023-08-17 17:07:43 +0900

    회원가입

--- src/main/java/com/education/onlinecampus/controller/lecture/MemberController.java
+++ src/main/java/com/education/onlinecampus/controller/lecture/MemberController.java
@@ -20,6 +20,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
